a7041fb6ccfd44f3435a1a17e116a0989b4e1c87
Solware <solwareve@gmail.com>
2025-07-08 23:40:09 -0400

    Fix Input component interface syntax error

--- src/shared/components/ui/input.tsx
+++ src/shared/components/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import { cn } from "@shared/lib/cn"
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   iconLeft?: React.ReactNode;
   iconRight?: React.ReactNode;
 }
