44ff9d249ec1be6d4c993574a1987cde4c0f8844
Solware <solwareve@gmail.com>
2025-07-08 23:38:16 -0400

    Fix InputProps interface syntax error

--- src/shared/components/ui/input.tsx
+++ src/shared/components/ui/input.tsx
@@ -3,11 +3,11 @@ import * as React from "react"
 import { cn } from "@shared/lib/cn"
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   iconLeft?: React.ReactNode;
   iconRight?: React.ReactNode;
 }
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+}
   ({ className, type, iconLeft, iconRight, ...props }, ref) => {
     return (
       <div className="relative">
