156905f25818b265f1234e7b6652543af1d5a914
Solware <solwareve@gmail.com>
2025-07-08 23:36:37 -0400

    Updated input.tsx

--- src/shared/components/ui/input.tsx
+++ src/shared/components/ui/input.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 
 import { cn } from "@shared/lib/cn"
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   iconLeft?: React.ReactNode;
   iconRight?: React.ReactNode;
 }
